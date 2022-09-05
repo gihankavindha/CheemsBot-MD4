52836766fad5eeb02bc0ad253dfb235ef716c63c
gihankavindha <112309347+gihankavindha@users.noreply.github.com>
2022-09-05 12:24:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@
 //i give permission to do as long as you
 //put Xeons youtube channel link in the video
 //description and tag me on githuh repo, 
-//thank you🦄
+//සතූතියි🦄
 //════════════════════════════//
 
 const fs = require('fs')
